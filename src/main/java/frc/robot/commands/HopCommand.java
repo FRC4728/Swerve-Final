611e55c845e6ffbeef310167caa2bcc6559c48ec
rocorirobotics@gmail.com <frc4728@gmail.com>
2023-02-13 17:25:43 -0600

    Added Hopper

---
+++ src/main/java/frc/robot/commands/HopCommand.java
@@ -0,0 +1,36 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.HopperSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class HopCommand extends CommandBase {
+    private HopperSubsystem s_Hopper;
+
+
+
+    public HopCommand(HopperSubsystem s_Hopper) {
+        this.s_Hopper = s_Hopper;
+        addRequirements(s_Hopper);
+
+
+    }
+
+    @Override
+    public void execute() {
+        s_Hopper.AlternateHopper();
+
+        /* Drive */
+
+    }
+
+    public void end(boolean interrupted) {
+        // when command ends, stop motors here
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
