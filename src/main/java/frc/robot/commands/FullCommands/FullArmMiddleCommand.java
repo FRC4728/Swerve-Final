fe924e419118585421da7bb487a5d398de619a0d
rocorirobotics@gmail.com <frc4728@gmail.com>
2023-02-17 19:10:25 -0600

    FullArmMiddle2

--- src/main/java/frc/robot/commands/FullCommands/FullArmMiddleCommand.java
+++ src/main/java/frc/robot/commands/FullCommands/FullArmMiddleCommand.java
@@ -2,24 +2,31 @@ package frc.robot.commands.FullCommands;
 
 import frc.robot.commands.ArmCommands.ArmMiddleCommand;
 import frc.robot.commands.ExtendCommands.ArmExtendCommand;
+import frc.robot.commands.ExtendCommands.ArmRetractCommand;
 import frc.robot.commands.ExtendCommands.FinalArmOut;
+import frc.robot.commands.HandCommands.HandOutConeCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ExtendingSubsystem;
+import frc.robot.subsystems.HandSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class FullArmMiddleCommand extends CommandBase {
     private ArmSubsystem s_Arm;
     private ExtendingSubsystem s_Extend;
+    private HandSubsystem s_Hand;
 
 
 
-    public FullArmMiddleCommand(ArmSubsystem s_Arm, ExtendingSubsystem s_Extend) {
+    public FullArmMiddleCommand(ArmSubsystem s_Arm, ExtendingSubsystem s_Extend, HandSubsystem s_Hand) {
         this.s_Arm = s_Arm;
         addRequirements(s_Arm);
 
         this.s_Extend = s_Extend;
         addRequirements(s_Extend);
+
+        this.s_Hand = s_Hand;
+        addRequirements(s_Hand);
 
     }
 
@@ -32,8 +39,10 @@ public class FullArmMiddleCommand extends CommandBase {
         // Add in command to be executed
         
         new SequentialCommandGroup(
+            new ArmRetractCommand(s_Extend),
             new ArmMiddleCommand(s_Arm),
-            new ArmExtendCommand(s_Extend)
+            new ArmExtendCommand(s_Extend),
+            new HandOutConeCommand(s_Hand)
             
         );
 
